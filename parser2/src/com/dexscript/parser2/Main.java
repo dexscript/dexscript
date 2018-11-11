2ed067f42bb6c572c2029b67490b52dea14e3259
Tao Wen <taowen@gmail.com>
2018-11-11 23:10:21 +0800

    shadowJar only include .class file

--- parser2/src/com/dexscript/parser2/Main.java
+++ parser2/src/com/dexscript/parser2/Main.java
@@ -1,7 +1,9 @@
 package com.dexscript.parser2;
 
+import com.dexscript.psi.impl.DexPackageClauseImpl;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new MyDexPackageClause());
+        System.out.println(new DexPackageClauseImpl(null));
     }
 }
