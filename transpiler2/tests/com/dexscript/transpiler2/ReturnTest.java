5b98b1941a0bba187dc6f4fd240d0417c560a618
Tao Wen <taowen@gmail.com>
2018-11-22 15:06:03 +0800

    add test for return value

--- transpiler2/tests/com/dexscript/transpiler2/ReturnTest.java
+++ transpiler2/tests/com/dexscript/transpiler2/ReturnTest.java
@@ -1,11 +1,18 @@
 package com.dexscript.transpiler2;
 
+import com.dexscript.runtime.Result;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ReturnTest {
 
     @Test
     public void return_string() {
-
+        String src = "" +
+                "function Hello(): string {\n" +
+                "   return 'hello'\n" +
+                "}";
+        Result res = TranspileOne.__(src);
+        Assert.assertEquals("hello", res.value());
     }
 }
--- transpiler2/tests/com/dexscript/transpiler2/TranspileOne.java
+++ transpiler2/tests/com/dexscript/transpiler2/TranspileOne.java
@@ -1,15 +1,17 @@
 package com.dexscript.transpiler2;
 
+import com.dexscript.runtime.Result;
+
 import java.util.Map;
 
 public interface TranspileOne {
-    static Object __(String src) {
+    static Result __(String src) {
         try {
 
             Map<String, Class<?>> transpiled = new Town()
                     .addFile("hello.ds", "package abc\n" + src)
                     .transpile();
-            return transpiled.get("abc.Hello").getConstructor().newInstance();
+            return (Result) transpiled.get("abc.Hello").getConstructor().newInstance();
         } catch (RuntimeException e) {
             throw e;
         } catch (Exception e) {
