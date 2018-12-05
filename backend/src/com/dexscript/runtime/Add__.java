bdfca5490c527c4f4116529308992ca156ca3f36
Tao Wen <taowen@gmail.com>
2018-12-05 14:40:27 +0800

    remove old backend

--- backend/src/com/dexscript/runtime/Add__.java
+++
@@ -1,8 +0,0 @@
-package com.dexscript.runtime;
-
-public class Add__ {
-
-    public static String String_String(String left, String right) {
-        return left + right;
-    }
-}
--- backend/src/com/dexscript/runtime/Result.java
+++
@@ -1,6 +0,0 @@
-package com.dexscript.runtime;
-
-public interface Result {
-    boolean finished();
-    Object value();
-}
