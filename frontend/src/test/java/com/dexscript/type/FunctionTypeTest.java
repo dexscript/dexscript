4ba48981d4696bb753a156a7159ae74c92ee62d8
Tao Wen <taowen@gmail.com>
2018-12-28 23:33:02 +0800

    migrate test

--- frontend/src/test/java/com/dexscript/type/FunctionTypeTest.java
+++ frontend/src/test/java/com/dexscript/type/FunctionTypeTest.java
@@ -1,12 +1,17 @@
 package com.dexscript.type;
 
 import com.dexscript.ast.DexActor;
+import com.dexscript.test.framework.FluentAPI;
+import com.dexscript.test.framework.Row;
+import com.dexscript.test.framework.Table;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.Collections;
+
+import static com.dexscript.test.framework.TestFramework.stripQuote;
+import static com.dexscript.test.framework.TestFramework.testDataFromMySection;
 
 public class FunctionTypeTest {
 
@@ -17,44 +22,34 @@ public class FunctionTypeTest {
         ts = new TypeSystem();
     }
 
-    private FunctionType func(String src) {
-        DexActor actor = DexActor.$("function " + src);
-        FunctionSig sig = new FunctionSig(ts, actor.sig());
-        return new FunctionType(ts, actor.functionName(), sig.params(), sig.ret(), sig);
-    }
-
     @Test
-    public void assignable() {
-        FunctionType hello1 = func("hello(): string");
-        FunctionType hello2 = func("hello(): string");
-        TestAssignable.$(true, hello1, hello2);
-        TestAssignable.$(true, hello2, hello1);
-        TestAssignable.$(false, hello2, ts.STRING);
+    public void same_function_is_assignable() {
+        testAssignable();
     }
 
     @Test
     public void name_not_assignable() {
-        TestAssignable.$(false, func("hello(): string"), func("world(): string"));
+        testAssignable();
     }
 
     @Test
     public void params_count_not_assignable() {
-        TestAssignable.$(false, func("hello(): string"), func("hello(arg0: string): string"));
+        testAssignable();
     }
 
     @Test
     public void param_name_not_assignable() {
-        TestAssignable.$(false, func("hello(a: int64)"), func("hello(b: int64)"));
+        testAssignable();
     }
 
     @Test
     public void param_type_not_assignable() {
-        TestAssignable.$(false, func("hello(arg0: int64)"), func("hello(arg0: string)"));
+        testAssignable();
     }
 
     @Test
     public void ret_not_assignable() {
-        TestAssignable.$(false, func("hello(): string"), func("hello(): void"));
+        testAssignable();
     }
 
     @Test
@@ -85,5 +80,24 @@ public class FunctionTypeTest {
     @Test
     public void test_to_string() {
         Assert.assertEquals("Hello(arg0: string): void", func("Hello(arg0: string): void").toString());
+    }
+
+    private FunctionType func(String src) {
+        DexActor actor = DexActor.$("function " + src);
+        FunctionSig sig = new FunctionSig(ts, actor.sig());
+        return new FunctionType(ts, actor.functionName(), sig.params(), sig.ret(), sig);
+    }
+
+    private void testAssignable() {
+        FluentAPI testData = testDataFromMySection();
+        Table table = testData.table();
+        for (Row row : table.body) {
+            boolean isAssignable = "true".equals(row.get(0));
+            String toSrc = stripQuote(row.get(1));
+            DType to = toSrc.contains(":") ? func(toSrc) : ResolveType.$(ts, toSrc);
+            String fromSrc = stripQuote(row.get(2));
+            DType from = fromSrc.contains(":") ? func(fromSrc) : ResolveType.$(ts, fromSrc);
+            TestAssignable.$(isAssignable, to, from);
+        }
     }
 }
---
+++ frontend/src/test/java/com/dexscript/type/FunctionTypeTest.md
@@ -0,0 +1,45 @@
+# same_function_is_assignable
+
+
+| assignable | to                | from              |
+| ---------- | ----------------- | ----------------- |
+| true       | `hello(): string` | `hello(): string` |
+| true       | `hello(): string` | `hello(): string` |
+| false      | `hello(): string` | `string`          |
+
+# name_not_assignable
+
+
+| assignable | to                | from              |
+| ---------- | ----------------- | ----------------- |
+| false      | `hello(): string` | `world(): string` |
+| false      | `world(): string` | `hello(): string` |
+
+# params_count_not_assignable
+
+| assignable | to                            | from                          |
+| ---------- | ----------------------------- | ----------------------------- |
+| false      | `hello(): string`             | `hello(arg0: string): string` |
+| false      | `hello(arg0: string): string` | `hello(): string`             |
+
+# param_name_not_assignable
+
+
+| assignable | to                | from              |
+| ---------- | ----------------- | ----------------- |
+| false      | `hello(a: int64)` | `hello(b: int64)` |
+| false      | `hello(b: int64)` | `hello(a: int64)` |
+
+# param_type_not_assignable
+
+| assignable | to                | from              |
+| ---------- | ----------------- | ----------------- |
+| false      | `hello(a: int64)` | `hello(a: string)` |
+| false      | `hello(a: string)` | `hello(a: int64)` |
+
+# ret_not_assignable
+
+| assignable | to                | from              |
+| ---------- | ----------------- | ----------------- |
+| false      | `hello(): int64` | `hello(): string` |
+| false      | `hello(): string` | `hello(): int64` |
