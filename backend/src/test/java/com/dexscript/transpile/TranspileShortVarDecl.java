cc4638dee7213bd78fe554cdc2834e3cdf76cbbd
Tao Wen <taowen@gmail.com>
2018-12-31 00:05:02 +0800

    migrate test

--- backend/src/test/java/com/dexscript/transpile/TranspileShortVarDecl.java
+++ backend/src/test/java/com/dexscript/transpile/TranspileShortVarDecl.java
@@ -1,27 +1,17 @@
 package com.dexscript.transpile;
 
-import org.junit.Assert;
+import com.dexscript.test.framework.TestFramework;
 import org.junit.Test;
 
 public class TranspileShortVarDecl {
 
     @Test
     public void string_const_widen_to_string() {
-        Object result = Transpile.$("" +
-                "function Hello(): string {" +
-                "   msg := 'hello'\n" +
-                "   return msg\n" +
-                "}");
-        Assert.assertEquals("hello", result);
+        TestFramework.assertByList(Transpile::$);
     }
 
     @Test
     public void integer_const_widen_to_int64() {
-        Long result = (Long) Transpile.$("" +
-                "function Hello(): int64 {" +
-                "   val := 100\n" +
-                "   return val\n" +
-                "}");
-        Assert.assertEquals(Long.valueOf(100), result);
+        TestFramework.assertByList(Transpile::$);
     }
 }
---
+++ backend/src/test/java/com/dexscript/transpile/TranspileShortVarDecl.md
@@ -0,0 +1,22 @@
+# string_const_widen_to_string
+
+```dexscript
+function Hello(): string {
+    msg := 'hello'
+    return msg
+}
+```
+
+* "hello"
+
+# integer_const_widen_to_int64
+
+```dexscript
+function Hello(): int64 {
+    val := 100
+    return val
+}
+```
+
+* 100
+
