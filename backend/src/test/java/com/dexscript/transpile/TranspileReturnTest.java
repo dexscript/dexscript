297475943207c943140bb1f0224371f91d0c3178
Tao Wen <taowen@gmail.com>
2018-12-30 23:01:56 +0800

    migrate test

--- backend/src/test/java/com/dexscript/transpile/TranspileReturnTest.java
+++ backend/src/test/java/com/dexscript/transpile/TranspileReturnTest.java
@@ -1,7 +1,6 @@
 package com.dexscript.transpile;
 
 import com.dexscript.test.framework.TestFramework;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class TranspileReturnTest {
@@ -12,29 +11,17 @@ public class TranspileReturnTest {
     }
 
     @Test
-    public void return_integer_const_as_integer_literal() {
-        Long result = (Long) Transpile.$("" +
-                "function Hello(): 1 {\n" +
-                "   return 1\n" +
-                "}");
-        Assert.assertEquals(Long.valueOf(1), result);
+    public void return_integer_literal() {
+        TestFramework.assertByList(Transpile::$);
     }
 
     @Test
-    public void return_integer_const_as_int64() {
-        Long result = (Long) Transpile.$("" +
-                "function Hello(): int64 {\n" +
-                "   return 1\n" +
-                "}");
-        Assert.assertEquals(Long.valueOf(1), result);
+    public void return_int64() {
+        TestFramework.assertByList(Transpile::$);
     }
 
     @Test
-    public void return_integer_const_as_int32() {
-        Integer result = (Integer) Transpile.$("" +
-                "function Hello(): int32 {\n" +
-                "   return 1\n" +
-                "}");
-        Assert.assertEquals(Integer.valueOf(1), result);
+    public void return_int32() {
+        TestFramework.assertByList(Transpile::$);
     }
 }
--- backend/src/test/java/com/dexscript/transpile/TranspileReturnTest.md
+++ backend/src/test/java/com/dexscript/transpile/TranspileReturnTest.md
@@ -10,3 +10,43 @@ function Hello(): string {
 * getClass
     * getSimpleName
         * "String"
+
+# return_integer_literal
+
+```dexscript
+function Hello(): 1 {
+   return 1
+}
+```
+
+* "1"
+* getClass
+    * getSimpleName
+        * "Long"
+
+# return_int64
+
+```dexscript
+function Hello(): int64 {
+   return 1
+}
+```
+
+* "1"
+* getClass
+    * getSimpleName
+        * "Long"
+
+# return_int32
+
+```dexscript
+function Hello(): int32 {
+   return 1
+}
+```
+
+* "1"
+* getClass
+    * getSimpleName
+        * "Integer"
+
